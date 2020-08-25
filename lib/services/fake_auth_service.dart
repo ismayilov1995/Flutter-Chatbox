aac9dc89995d526628e637b6296c457bf5e0c35f
ismayilov1995 <ismayilov1995@gmail.com>
2020-08-25 18:24:01 +0400

    Global access fake and real backend call

--- lib/services/fake_auth_service.dart
+++ lib/services/fake_auth_service.dart
@@ -1,22 +1,24 @@
+import 'dart:html';
+
 import 'package:flutter_chatbox/models/user_model.dart';
 import 'package:flutter_chatbox/services/auth_base.dart';
 
 class FakeAuthService implements AuthBase {
+  String userID = "I7sm5a504y8I09l6";
+
   @override
-  Future<AppUser> currentUser() {
-    // TODO: implement currentUser
-    throw UnimplementedError();
+  Future<AppUser> currentUser() async {
+    return await Future.value(AppUser(userID: userID));
   }
 
   @override
-  Future<AppUser> signInAnonymous() {
-    // TODO: implement signInAnonymous
-    throw UnimplementedError();
+  Future<AppUser> signInAnonymous() async {
+    return await Future.delayed(
+        Duration(seconds: 2), () => AppUser(userID: userID));
   }
 
   @override
   Future<bool> signOut() {
-    // TODO: implement signOut
-    throw UnimplementedError();
+    return Future.value(true);
   }
 }
--- lib/services/firebase_auth_service.dart
+++ lib/services/firebase_auth_service.dart
@@ -1,19 +1,43 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_chatbox/models/user_model.dart';
 import 'package:flutter_chatbox/services/auth_base.dart';
 
 class FirebaseAuthService implements AuthBase {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
   @override
   Future<AppUser> currentUser() {
-    throw UnimplementedError();
+    try {
+      User user = _firebaseAuth.currentUser;
+      return Future.delayed(Duration.zero, () => _userFromFirebase(user));
+    } catch (e) {
+      print('XETA: $e');
+    }
+  }
+
+  AppUser _userFromFirebase(User user) {
+    return user == null ? null : AppUser(userID: user.uid);
   }
 
   @override
-  Future<AppUser> signInAnonymous() {
-    throw UnimplementedError();
+  Future<AppUser> signInAnonymous() async {
+    try {
+      UserCredential result = await FirebaseAuth.instance.signInAnonymously();
+      return _userFromFirebase(result.user);
+    } catch (e) {
+      print('XETA: $e');
+      return null;
+    }
   }
 
   @override
-  Future<bool> signOut() {
-    throw UnimplementedError();
+  Future<bool> signOut() async {
+    try {
+      await _firebaseAuth.signOut();
+      return true;
+    } catch (e) {
+      print('XETA: $e');
+      return false;
+    }
   }
 }
