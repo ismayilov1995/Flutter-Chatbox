01a62113019fc0fdbc34b07d9736d4a032a80ccf
ismayilov1995 <ismayilov1995@gmail.com>
2020-08-30 16:23:01 +0400

    after login navigation work

--- lib/app/sign_in/email_password_login_create.dart
+++ lib/app/sign_in/email_password_login_create.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chatbox/common_widget/social_login_button.dart';
-import 'package:flutter_chatbox/models/user_model.dart';
 import 'package:flutter_chatbox/viewmodel/user_model.dart';
 import 'package:provider/provider.dart';
 
@@ -12,89 +11,100 @@ class EmailLoginPage extends StatefulWidget {
 class _EmailLoginPageState extends State<EmailLoginPage> {
   bool isLoginMode = true;
   final _formKey = GlobalKey<FormState>();
-  String _email, _password, _passwordConfirm;
+  String _email, _password;
   TextEditingController _passCtrl = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    final _userVM = Provider.of<UserViewmodel>(context);
+    _passCtrl.text = '7090698';
+
+    if (_userVM.user != null) {
+      Future.delayed(
+          Duration(milliseconds: 20), () => Navigator.of(context).pop());
+    }
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(isLoginMode ? 'Login' : 'Register'),
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          margin: EdgeInsets.fromLTRB(12, 16, 12, 0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                TextFormField(
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.mail),
-                      hintText: "Email",
-                      labelText: "Email",
-                      border: OutlineInputBorder()),
-                  validator: (val) {
-                    return val.contains('@')
-                        ? null
-                        : 'Input valid email format';
-                  },
-                  onSaved: (val) => _email = val,
-                ),
-                SizedBox(
-                  height: 16,
-                ),
+        appBar: AppBar(
+          title: Text(isLoginMode ? 'Login' : 'Register'),
+        ),
+        body: _userVM.state == ViewState.IDLE
+            ? buildSingleChildScrollView(context)
+            : Center(child: CircularProgressIndicator()));
+  }
+
+  SingleChildScrollView buildSingleChildScrollView(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+        margin: EdgeInsets.fromLTRB(12, 16, 12, 0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                initialValue: "emre@emre.com",
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.mail),
+                    hintText: "Email",
+                    labelText: "Email",
+                    border: OutlineInputBorder()),
+                validator: (val) {
+                  return val.contains('@') ? null : 'Input valid email format';
+                },
+                onSaved: (val) => _email = val,
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              TextFormField(
+                obscureText: true,
+                controller: _passCtrl,
+                decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.security),
+                    hintText: "Password",
+                    labelText: "Password",
+                    border: OutlineInputBorder()),
+                validator: (val) {
+                  return val.length > 5 ? null : 'Minimum length 6';
+                },
+                onSaved: (val) => _password = val,
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              if (!isLoginMode) ...[
                 TextFormField(
                   obscureText: true,
-                  controller: _passCtrl,
                   decoration: InputDecoration(
                       prefixIcon: Icon(Icons.security),
-                      hintText: "Password",
-                      labelText: "Password",
+                      hintText: "Password confirm",
+                      labelText: "Password confirm",
                       border: OutlineInputBorder()),
                   validator: (val) {
-                    return val.length > 5 ? null : 'Minimum length 6';
+                    return val == _passCtrl.text
+                        ? null
+                        : 'Password not matched';
                   },
-                  onSaved: (val) => _password = val,
                 ),
                 SizedBox(
                   height: 16,
-                ),
-                if (!isLoginMode) ...[
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.security),
-                        hintText: "Password confirm",
-                        labelText: "Password confirm",
-                        border: OutlineInputBorder()),
-                    validator: (val) {
-                      return val == _passCtrl.text
-                          ? null
-                          : 'Password not matched';
-                    },
-                    onSaved: (val) => _passwordConfirm = val,
-                  ),
-                  SizedBox(
-                    height: 16,
-                  )
-                ],
-                SocialLoginButton(
-                  radius: 4.0,
-                  buttonText: isLoginMode ? 'Login' : 'Create Account',
-                  buttonColor: Theme.of(context).primaryColor,
-                  onPressed: () => _onSubmit(),
-                ),
-                FlatButton(
-                  child: Text(
-                    isLoginMode ? "Create Account" : 'Login exists account',
-                    style: TextStyle(color: Theme.of(context).primaryColor),
-                  ),
-                  onPressed: () => setState(() => isLoginMode = !isLoginMode),
                 )
               ],
-            ),
+              SocialLoginButton(
+                radius: 4.0,
+                buttonText: isLoginMode ? 'Login' : 'Create Account',
+                buttonColor: Theme.of(context).primaryColor,
+                onPressed: () => _onSubmit(),
+              ),
+              FlatButton(
+                child: Text(
+                  isLoginMode ? "Create Account" : 'Login exists account',
+                  style: TextStyle(color: Theme.of(context).primaryColor),
+                ),
+                onPressed: () => setState(() => isLoginMode = !isLoginMode),
+              )
+            ],
           ),
         ),
       ),
@@ -104,11 +114,12 @@ class _EmailLoginPageState extends State<EmailLoginPage> {
   void _onSubmit() async {
     if (_formKey.currentState.validate()) {
       _formKey.currentState.save();
-      UserViewmodel _userVM = Provider.of<UserViewmodel>(context, listen: false);
+      UserViewmodel _userVM =
+          Provider.of<UserViewmodel>(context, listen: false);
       if (isLoginMode) {
-        AppUser _loginUser = await _userVM.signInWithEmail(_email, _password);
+        await _userVM.signInWithEmail(_email, _password);
       } else {
-        AppUser _registerUser = await _userVM.createWithEmail(_email, _password);
+        await _userVM.createWithEmail(_email, _password);
       }
     }
   }
