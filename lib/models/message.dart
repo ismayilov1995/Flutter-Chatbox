6c6d7c640e299f2eace4915b167ba26427bbbd19
ismayilov1995 <ismayilov1995@gmail.com>
2020-09-01 17:01:36 +0400

    Create Message model class

---
+++ lib/models/message.dart
@@ -0,0 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Message {
+  final String from;
+  final String to;
+  final bool fromMe;
+  final String message;
+  final DateTime createdAt;
+
+  Message({this.from, this.to, this.fromMe, this.message, this.createdAt});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'from': from,
+      'to': to,
+      'fromMe': fromMe,
+      'message': message,
+      'createdAt': createdAt,
+    };
+  }
+
+  Message.fromMap(Map<String, dynamic> map)
+      : from = map['from'],
+        to = map['ro'],
+        fromMe = map['fromMe'],
+        message = map['message'],
+        createdAt = (map['createdAt'] as Timestamp).toDate();
+
+  @override
+  String toString() {
+    return 'Message{from: $from, to: $to, fromMe: $fromMe, message: $message, createdAt: $createdAt}';
+  }
+}
