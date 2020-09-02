5aa9c7391f9577d0684a92034d321ef72e6b14ab
ismayilov1995 <ismayilov1995@gmail.com>
2020-09-02 20:23:26 +0400

    Create Users ViewModel 3

--- lib/app/users.dart
+++ lib/app/users.dart
@@ -39,14 +39,35 @@ class _UsersPageState extends State<UsersPage> {
               case UsersViewState.BUSY:
                 return Center(child: CircularProgressIndicator());
               case UsersViewState.LOADED:
-                return ListView.builder(
-                    controller: _scrollController,
-                    itemCount: model.users.length,
-                    itemBuilder: (context, index) {
-                      AppUser user = model.users[index];
-                      // if (index == _users.length - 1) return _loadingIndicator();
-                      return _userListItem(index);
-                    });
+                if (model.users.length == 1)
+                  return Center(
+                      child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        "No one use this app, you're first 🙃",
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      FlatButton.icon(
+                          onPressed: model.onRefresh,
+                          icon: Icon(Icons.refresh),
+                          label: Text('Refresh'))
+                    ],
+                  ));
+                return RefreshIndicator(
+                  onRefresh: model.onRefresh,
+                  child: ListView.builder(
+                      controller: _scrollController,
+                      itemCount: model.users.length,
+                      itemBuilder: (context, index) {
+                        if (index == model.users.length - 1 &&
+                            model.hasMoreLoading) return _loadingIndicator();
+                        return _userListItem(index);
+                      }),
+                );
               default:
                 return Center(
                   child: Text('Error'),
@@ -56,72 +77,13 @@ class _UsersPageState extends State<UsersPage> {
         ));
   }
 
-  // void getUsers() async {
-  //   final _userVM = Provider.of<UserViewmodel>(context, listen: false);
-  //   if (!_hasMore) return;
-  //   if (_isLoading) return;
-  //   setState(() {
-  //     _isLoading = true;
-  //   });
-  //   List<AppUser> users = await _userVM.getPaginatedUsers(_lastUser, _limit);
-  //   if (_lastUser == null) {
-  //     _users = [];
-  //     _users.addAll(users);
-  //   } else {
-  //     _users.addAll(users);
-  //   }
-  //   if (users.length < _limit) _hasMore = false;
-  //   _lastUser = _users.last;
-  //   setState(() {
-  //     _isLoading = false;
-  //   });
-  // }
-
-  // Widget _generateUsersList() {
-  //   if (_users.length <= 1)
-  //     return Center(
-  //         child: Column(
-  //       mainAxisSize: MainAxisSize.min,
-  //       children: [
-  //         Text(
-  //           "No one use this app, you're first 🙃",
-  //           style: Theme.of(context).textTheme.headline6,
-  //         ),
-  //         SizedBox(
-  //           height: 16,
-  //         ),
-  //         FlatButton.icon(
-  //             onPressed: _onRefresh,
-  //             icon: Icon(Icons.refresh),
-  //             label: Text('Refresh'))
-  //       ],
-  //     ));
-  //   final _userVM = Provider.of<UserViewmodel>(context, listen: false);
-  //   int userIndex =
-  //       _users.indexWhere((element) => element.userID == _userVM.user.userID);
-  //   if (userIndex >= 0) _users.removeAt(userIndex);
-  //   return RefreshIndicator(onRefresh: _onRefresh, child: null);
-  // }
-
   Widget _loadingIndicator() {
-    return _isLoading
-        ? Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Center(
-              child: Opacity(
-                  opacity: _isLoading ? 1 : 0,
-                  child: CircularProgressIndicator()),
-            ),
-          )
-        : null;
-  }
-
-  Future<void> _onRefresh() async {
-    // _lastUser = null;
-    // _hasMore = true;
-    // getUsers();
-    await Future.delayed(Duration(seconds: 1));
-    return;
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
   }
 
   Widget _userListItem(int index) {
--- lib/viewmodel/users_viewmodel.dart
+++ lib/viewmodel/users_viewmodel.dart
@@ -10,6 +10,7 @@ class UsersViewmodel with ChangeNotifier {
   List<AppUser> _users;
   UsersViewState _state = UsersViewState.IDLE;
   AppUser _lastUser;
+  bool _hasMore = true;
   static final _limit = 10;
 
   UserRepository _userRepository = locator<UserRepository>();
@@ -17,27 +18,40 @@ class UsersViewmodel with ChangeNotifier {
   UsersViewmodel() {
     _users = [];
     _lastUser = null;
-    getPaginatedUsers(_lastUser, _limit);
+    getPaginatedUsers(_lastUser, _limit, true);
   }
 
   List<AppUser> get users => _users;
 
   UsersViewState get state => _state;
 
+  bool get hasMoreLoading => _hasMore;
+
   set state(UsersViewState value) {
     _state = value;
     notifyListeners();
   }
 
-  Future<void> getPaginatedUsers(AppUser lastUser, int limit) async {
+  Future<void> getPaginatedUsers(
+      AppUser lastUser, int limit, bool isFirstLoad) async {
     if (_users.length > 0) _lastUser = _users.last;
-    state = UsersViewState.BUSY;
-    _users = await _userRepository.getPaginatedUsers(lastUser, limit);
+    if (isFirstLoad) state = UsersViewState.BUSY;
+    var newList = await _userRepository.getPaginatedUsers(_lastUser, limit);
+    if (newList.length < limit) _hasMore = false;
+    _users.addAll(newList);
     state = UsersViewState.LOADED;
-    _users.forEach((element) => print(element.username));
   }
 
   Future<void> getMoreUsers() async {
-    await getPaginatedUsers(_lastUser, _limit);
+    if (_hasMore) await getPaginatedUsers(_lastUser, _limit, false);
+  }
+
+  Future<void> onRefresh() async {
+    _lastUser = null;
+    _hasMore = true;
+    _users = [];
+    getPaginatedUsers(_lastUser, _limit, false);
+    await Future.delayed(Duration(seconds: 1));
+    return;
   }
 }
